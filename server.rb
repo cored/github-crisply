e5912989f9796f86cf48bb34dd81c5eb72e82ae6
Rafael George <george.rafael@gmail.com>
2012-08-11 17:18:41 +0000

    Checking server bootup

--- server.rb
+++ server.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 require 'sinatra/base'
 
 class Server < Sinatra::Base
+   get '/' do 
+     "Server running"
+   end
+
    post '/' do 
      @payload = params[:payload]
      "It worked"
